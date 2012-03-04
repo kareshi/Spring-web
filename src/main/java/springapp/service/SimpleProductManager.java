aa4a557eb707ac3a282685e448f9aa36ebaf840b
Ivan <kareshi@gmail.com>
2012-03-04 21:29:04 +0800

    Remove the fake on getProducts.

--- src/main/java/springapp/service/SimpleProductManager.java
+++ src/main/java/springapp/service/SimpleProductManager.java
@@ -1,21 +1,24 @@
 package springapp.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import springapp.domain.Product;
 
 public class SimpleProductManager implements ProductManager {
 
+    private List<Product> products;
+
     public List<Product> getProducts() {
-       return null;
+        return products;
     }
 
     public void increasePrice(int percentage) {
-        throw new UnsupportedOperationException();
+        // TODO Auto-generated method stub      
     }
 
     public void setProducts(List<Product> products) {
-        throw new UnsupportedOperationException();
+        this.products = products;
     }
 
 }
